45d663bf1cfe970cba05e7543661541f068a7906
andrew-pstu <andrew-pstu@mail.ru>
2019-07-28 22:03:13 +0500

    Init

---
+++ Form1.Designer.vb
@@ -0,0 +1,337 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.AxMSComm1 = New AxMSCommLib.AxMSComm()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.TextBox11 = New System.Windows.Forms.TextBox()
+        Me.TextBox12 = New System.Windows.Forms.TextBox()
+        Me.Label26 = New System.Windows.Forms.Label()
+        Me.Label27 = New System.Windows.Forms.Label()
+        Me.Label28 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox13 = New System.Windows.Forms.TextBox()
+        Me.TextBox14 = New System.Windows.Forms.TextBox()
+        Me.Label22 = New System.Windows.Forms.Label()
+        Me.TextBox15 = New System.Windows.Forms.TextBox()
+        Me.Label23 = New System.Windows.Forms.Label()
+        Me.TextBox16 = New System.Windows.Forms.TextBox()
+        Me.Label24 = New System.Windows.Forms.Label()
+        Me.TextBox17 = New System.Windows.Forms.TextBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        CType(Me.AxMSComm1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxMSComm1
+        '
+        Me.AxMSComm1.Enabled = True
+        Me.AxMSComm1.Location = New System.Drawing.Point(6, 370)
+        Me.AxMSComm1.Name = "AxMSComm1"
+        Me.AxMSComm1.OcxState = CType(resources.GetObject("AxMSComm1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxMSComm1.Size = New System.Drawing.Size(38, 38)
+        Me.AxMSComm1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 43)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(101, 20)
+        Me.TextBox1.TabIndex = 19
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(6, 69)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(101, 20)
+        Me.TextBox2.TabIndex = 20
+        Me.TextBox2.Text = "0"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(113, 55)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(129, 29)
+        Me.TextBox3.TabIndex = 21
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(298, 241)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(45, 13)
+        Me.Label17.TabIndex = 25
+        Me.Label17.Text = "Предел"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(68, 320)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(214, 32)
+        Me.Button1.TabIndex = 28
+        Me.Button1.Text = " измерение"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox10.Location = New System.Drawing.Point(258, 55)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(122, 29)
+        Me.TextBox10.TabIndex = 41
+        Me.TextBox10.Text = "0"
+        '
+        'TextBox11
+        '
+        Me.TextBox11.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox11.Location = New System.Drawing.Point(396, 55)
+        Me.TextBox11.Name = "TextBox11"
+        Me.TextBox11.Size = New System.Drawing.Size(123, 29)
+        Me.TextBox11.TabIndex = 42
+        Me.TextBox11.Text = "0"
+        '
+        'TextBox12
+        '
+        Me.TextBox12.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox12.Location = New System.Drawing.Point(381, 181)
+        Me.TextBox12.Name = "TextBox12"
+        Me.TextBox12.Size = New System.Drawing.Size(210, 29)
+        Me.TextBox12.TabIndex = 43
+        '
+        'Label26
+        '
+        Me.Label26.AutoSize = True
+        Me.Label26.Location = New System.Drawing.Point(233, 39)
+        Me.Label26.Name = "Label26"
+        Me.Label26.Size = New System.Drawing.Size(147, 13)
+        Me.Label26.TabIndex = 44
+        Me.Label26.Text = "отфильтрованное значение"
+        '
+        'Label27
+        '
+        Me.Label27.AutoSize = True
+        Me.Label27.Location = New System.Drawing.Point(338, 303)
+        Me.Label27.Name = "Label27"
+        Me.Label27.Size = New System.Drawing.Size(238, 13)
+        Me.Label27.TabIndex = 45
+        Me.Label27.Text = "измеряемое сопротивление при 20 С, Ом/км"
+        '
+        'Label28
+        '
+        Me.Label28.AutoSize = True
+        Me.Label28.Location = New System.Drawing.Point(298, 192)
+        Me.Label28.Name = "Label28"
+        Me.Label28.Size = New System.Drawing.Size(26, 13)
+        Me.Label28.TabIndex = 46
+        Me.Label28.Text = "Ток"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(68, 380)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(214, 28)
+        Me.Button3.TabIndex = 48
+        Me.Button3.Text = "СТОП"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox13
+        '
+        Me.TextBox13.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox13.Location = New System.Drawing.Point(381, 230)
+        Me.TextBox13.Name = "TextBox13"
+        Me.TextBox13.Size = New System.Drawing.Size(210, 29)
+        Me.TextBox13.TabIndex = 60
+        '
+        'TextBox14
+        '
+        Me.TextBox14.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox14.Location = New System.Drawing.Point(97, 234)
+        Me.TextBox14.Name = "TextBox14"
+        Me.TextBox14.Size = New System.Drawing.Size(101, 29)
+        Me.TextBox14.TabIndex = 61
+        '
+        'Label22
+        '
+        Me.Label22.AutoSize = True
+        Me.Label22.Location = New System.Drawing.Point(12, 241)
+        Me.Label22.Name = "Label22"
+        Me.Label22.Size = New System.Drawing.Size(72, 13)
+        Me.Label22.TabIndex = 62
+        Me.Label22.Text = "температура"
+        '
+        'TextBox15
+        '
+        Me.TextBox15.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox15.Location = New System.Drawing.Point(396, -2)
+        Me.TextBox15.Name = "TextBox15"
+        Me.TextBox15.Size = New System.Drawing.Size(101, 29)
+        Me.TextBox15.TabIndex = 63
+        Me.TextBox15.Text = "20"
+        '
+        'Label23
+        '
+        Me.Label23.AutoSize = True
+        Me.Label23.Location = New System.Drawing.Point(359, 9)
+        Me.Label23.Name = "Label23"
+        Me.Label23.Size = New System.Drawing.Size(31, 13)
+        Me.Label23.TabIndex = 64
+        Me.Label23.Text = "окно"
+        '
+        'TextBox16
+        '
+        Me.TextBox16.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox16.Location = New System.Drawing.Point(578, -2)
+        Me.TextBox16.Name = "TextBox16"
+        Me.TextBox16.Size = New System.Drawing.Size(101, 29)
+        Me.TextBox16.TabIndex = 65
+        '
+        'Label24
+        '
+        Me.Label24.AutoSize = True
+        Me.Label24.Location = New System.Drawing.Point(553, 9)
+        Me.Label24.Name = "Label24"
+        Me.Label24.Size = New System.Drawing.Size(19, 13)
+        Me.Label24.TabIndex = 66
+        Me.Label24.Text = "a="
+        '
+        'TextBox17
+        '
+        Me.TextBox17.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox17.Location = New System.Drawing.Point(97, 269)
+        Me.TextBox17.Name = "TextBox17"
+        Me.TextBox17.Size = New System.Drawing.Size(101, 29)
+        Me.TextBox17.TabIndex = 67
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(68, 463)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(65, 17)
+        Me.RadioButton1.TabIndex = 68
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Канал 1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(68, 486)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(65, 17)
+        Me.RadioButton2.TabIndex = 69
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Канал 0"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(501, 319)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(90, 82)
+        Me.Button2.TabIndex = 70
+        Me.Button2.Text = "Новый файл"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox4.Location = New System.Drawing.Point(341, 319)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(123, 29)
+        Me.TextBox4.TabIndex = 71
+        Me.TextBox4.Text = "0,02405"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(874, 601)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.TextBox17)
+        Me.Controls.Add(Me.Label24)
+        Me.Controls.Add(Me.TextBox16)
+        Me.Controls.Add(Me.Label23)
+        Me.Controls.Add(Me.TextBox15)
+        Me.Controls.Add(Me.Label22)
+        Me.Controls.Add(Me.TextBox14)
+        Me.Controls.Add(Me.TextBox13)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label28)
+        Me.Controls.Add(Me.Label27)
+        Me.Controls.Add(Me.Label26)
+        Me.Controls.Add(Me.TextBox12)
+        Me.Controls.Add(Me.TextBox11)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.AxMSComm1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.AxMSComm1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents AxMSComm1 As AxMSCommLib.AxMSComm
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents TextBox12 As TextBox
+    Friend WithEvents Label26 As Label
+    Friend WithEvents Label27 As Label
+    Friend WithEvents Label28 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox13 As TextBox
+    Friend WithEvents TextBox14 As TextBox
+    Friend WithEvents Label22 As Label
+    Friend WithEvents TextBox15 As TextBox
+    Friend WithEvents Label23 As Label
+    Friend WithEvents TextBox16 As TextBox
+    Friend WithEvents Label24 As Label
+    Friend WithEvents TextBox17 As TextBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox4 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,188 @@
+﻿Imports System.ComponentModel
+
+Public Class Form1
+    Public Ba() As Byte
+    Public Калибровка_Напряжение(0 To 2, 0 To 7) As Long
+    Public Калибровка_Температура(0 To 2, 0 To 7) As Long
+    Public Input_Range As Byte
+    Public Output_Current As Byte
+    Public N_channel As Byte
+    Public Control_byte(0) As Byte
+    Public Y1 As Integer
+    Public Y1_prev As Integer
+    Public N As Integer ' число проходов фильтра
+    Public a As Double ' коэф фильтра
+    Public Iteraciya As Integer
+    Public Start_Flag As Boolean
+    Public Measured_value_ch1 As Long ' переменная результата измерения канала1
+    Public Measured_value_ch0 As Long ' переменная результата измерения канала2
+    Public Флаг_готовности_результата_измерения As Boolean
+    Public Результат_АЦП_канал0_прямой_ток As Long
+    Public Результат_АЦП_канал1_прямой_ток As Long
+    Public Результат_АЦП_канал0_обратный_ток As Long
+    Public Результат_АЦП_канал1_обратный_ток As Long
+    Public Температура As Long
+    Public Stop_Flag As Boolean
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ReDim Ba(0 To 7)
+        Флаг_готовности_результата_измерения = False
+        AxMSComm1.InputMode = MSCommLib.InputModeConstants.comInputModeBinary
+        AxMSComm1.PortOpen = True
+        RadioButton1.Checked = True
+        Stop_Flag = False
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        If AxMSComm1.PortOpen = True Then AxMSComm1.PortOpen = False
+    End Sub
+
+    Private Sub AxMSComm1_OnComm(sender As Object, e As EventArgs) Handles AxMSComm1.OnComm
+        ' принимаем первые 8 байт результата измерения (3 байта-прямой ток и 3 байта -обратный и 2 байта температуры)
+        Ba = AxMSComm1.Input
+        If Ba.Length < 8 Then Exit Sub
+        Результат_АЦП_канал1_прямой_ток = Ba(0) * 256 * 256 + Ba(1) * 256 + Ba(2)
+        Результат_АЦП_канал1_обратный_ток = Ba(3) * 256 * 256 + Ba(4) * 256 + Ba(5)
+        Measured_value_ch1 = Результат_АЦП_канал1_прямой_ток - Результат_АЦП_канал1_обратный_ток
+        Температура = Ba(6) * 256 + Ba(7)
+        Флаг_готовности_результата_измерения = True
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click ' кнопка "выполнить 1 измерение"
+        ReDim Калибровка_Напряжение(0 To 2, 0 To 7)
+        ReDim Калибровка_Температура(0 To 2, 0 To 7)
+        Dim Prev_Cur As Integer
+        Dim Prev_Range As Integer
+        'System.Threading.Thread.Sleep(3)
+        Input_Range = 7
+        Output_Current = 1
+        Stop_Flag = False
+        N = CInt(TextBox15.Text)
+
+        Do 'цикл измерений с перебором предела и тока
+            If Stop_Flag = True Then Exit Do
+            Измерение()
+            If (Measured_value_ch1 > 0.9 * 16777216) And (Output_Current = 1) And (Input_Range = 7) Then
+                Stop_Flag = True : Button3_Click(sender, e) : MsgBox("R слишком велико!")
+                Exit Sub
+            End If
+
+            a = 0.92 ' фильтр ослабляем для быстрейшего установления
+            Y1 = 0 : Y1_prev = 0 : Iteraciya = 0
+            Y1_prev = Measured_value_ch1 ' присваиваем предыдущему результату фильтрации текущее измеренное значение,
+            ' чтобы ползти к измеряемуму значению не от нуля, что быстрее
+            TextBox12.Text = Output_Current
+            TextBox13.Text = Input_Range
+
+            Цикл_измерений_с_фильтрацией()
+            Калибровка_Напряжение(Output_Current - 1, Input_Range) = Y1
+            Калибровка_Температура(Output_Current - 1, Input_Range) = Температура
+            'если измеренное значение меньше 1/2 предела и еще есть куда уменьшать предел, то уменьшить предел
+            If (Y1 <= 0.9 / 2 * 16777216) And (Input_Range <> 0) Then
+                Input_Range = Input_Range - 1
+            End If
+            'если диапазон уменьшать уже некуда, а измеренное значение меньше 1/10 предела и вых ток не на пределе, то увеличить ток
+            If (Prev_Range = Input_Range) Then
+                Input_Range = 7 : Измерение() 'меняем предел на макс, чтобы увидеть можно ли увеличивать ток
+                If (Measured_value_ch1 <= 0.09 * 16777216) And (Output_Current <> 3) Then
+                    Output_Current = Output_Current + 1
+                Else
+                    Input_Range = Prev_Range ' иначе восстановить предел
+                End If
+            End If
+
+            If (Prev_Cur = Output_Current) And (Prev_Range = Input_Range) Then
+                Exit Do
+            End If
+            Prev_Range = Input_Range
+            Prev_Cur = Output_Current
+        Loop
+        Button3_Click(sender, e) ' кнопка "СТОП"
+        Запись_в_файл()
+    End Sub
+
+    Private Sub Цикл_измерений_с_фильтрацией()
+        Dim Eps As Double
+        Do
+            If Stop_Flag = True Then Exit Do
+
+            TextBox16.Text = a
+            Iteraciya = Iteraciya + 1 ' счетчик проходов фильтра
+            Y1 = Y1_prev * a + Measured_value_ch1 * (1 - a)
+            TextBox10.Text = Y1 'вывод отфильтрованного значения
+            Eps = Math.Abs((Y1 - Y1_prev) / Y1)
+            Application.DoEvents()
+
+            Control_byte(0) = N_channel + Input_Range * 2 + Output_Current * 16
+            AxMSComm1.Output = Control_byte ' запуск измерения
+            Флаг_готовности_результата_измерения = False
+            Do While Флаг_готовности_результата_измерения = False ' ждем ответа от прибора
+                Application.DoEvents() ' даем системе обрабатывать события
+            Loop
+            Y1_prev = Y1
+            TextBox11.Text = Iteraciya
+            If Iteraciya > N / 2 Then a = 0.99 ' врубаем фильтр на полную, чтобы меньше колбасило результат
+            If Iteraciya > N * 2 Then a = 0.999 'если результат колбасит уже слишком долго, то затягиваем фильтр до предела
+            TextBox1.Text = Результат_АЦП_канал1_прямой_ток
+            TextBox2.Text = Результат_АЦП_канал1_обратный_ток
+            TextBox3.Text = Measured_value_ch1
+            TextBox14.Text = Format((Температура * 0.007153 - 254.1624), "00.0") 'Format(((Температура - 32768) / 256), "00.0") 'переводим результат АЦП показаний встроенного в ADuC824 термосенсора  в градусы цельсия
+            TextBox17.Text = Температура
+
+        Loop Until (Eps < 0.000001 And Iteraciya >= N) Or (Stop_Flag) ' если достигли заданной малости колебаний результата фильтра Eps > 0.0001 And 
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click ' кнопка "СТОП"
+        Application.DoEvents()
+        System.Threading.Thread.Sleep(500)
+        Application.DoEvents()
+        Stop_Flag = True
+        Выкл_источник_тока()
+    End Sub
+
+    Private Sub Измерение()
+        Control_byte(0) = N_channel + Input_Range * 2 + Output_Current * 16
+        AxMSComm1.Output = Control_byte ' запуск измерения
+        Флаг_готовности_результата_измерения = False
+        Do While Флаг_готовности_результата_измерения = False ' ждем ответа от прибора
+            Application.DoEvents() ' даем системе обрабатывать события
+        Loop
+    End Sub
+
+    Private Sub Выкл_источник_тока()
+        Output_Current = 0
+        Input_Range = CByte(7)
+        Control_byte(0) = 1 + Input_Range * 2 + Output_Current * 16
+        AxMSComm1.Output = Control_byte
+        Application.DoEvents()
+    End Sub
+
+    Private Sub Запись_в_файл()
+        If IO.File.Exists("test.txt") = False Then ' если Файл не существует
+            My.Computer.FileSystem.WriteAllText("test.txt", "", True) ' создаем пустой файл
+        End If
+
+        For i = 0 To 2
+            For j = 0 To 7
+                FileOpen(1, "test.txt", OpenMode.Append)
+                Print(1, TextBox4.Text + " " + CStr(i + 1) + " " + CStr(j) + " " + CStr(Калибровка_Напряжение(i, j)) + " " + CStr(Калибровка_Температура(i, j)) + vbNewLine)
+                FileClose(1)
+            Next j
+        Next i
+    End Sub
+
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        If RadioButton1.Checked = True Then
+            N_channel = 1
+        Else
+            N_channel = 0
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        My.Computer.FileSystem.WriteAllText("test.txt", "", False) ' создаем пустой файл
+    End Sub
+End Class
